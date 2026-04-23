c6bc013849216bf9eb9130212baa738c4bd7709f
Rohan5210 <rohan.choudhary@capsitech.com>
2026-04-23 17:32:03 +0530

    basic ui manager

--- Scripts/UIManager.gd
+++ Scripts/UIManager.gd
@@ -1,8 +1,14 @@
 extends Node
 
+@export var canvas_layers:Array[CanvasLayer]=[]
 
 func disable_canvas(layer:CanvasLayer):
 	layer.visible = false
 
+func disable_all_canvaslayers():
+	for item in canvas_layers:
+		item.visible=false
+
 func enable_canvas(layer:CanvasLayer):
+	disable_all_canvaslayers()
 	layer.visible = true
