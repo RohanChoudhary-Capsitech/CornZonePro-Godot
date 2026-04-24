3d95f755667bc6cfda67135d12cf3dbabbc21b6c
Rohan5210 <rohan.choudhary@capsitech.com>
2026-04-24 15:10:19 +0530

    timer added

---
+++ Scripts/timer_slider.gd
@@ -0,0 +1,42 @@
+extends TextureProgressBar
+
+@export var max_time:float=30.0
+@export var smooth_speed:float=30.0
+
+var current_time:float
+var display_value:float
+
+func _ready() -> void:
+	current_time=max_time
+	display_value=max_time
+	
+	max_value=max_time
+	value=display_value
+	
+	fill_mode=TextureProgressBar.FILL_LEFT_TO_RIGHT
+
+
+func _process(delta: float) -> void:
+	#Real Timer
+	if current_time>0:
+		current_time-=delta
+		current_time=max(current_time,0)
+		
+	#Smooth UI
+	display_value=lerp(display_value,current_time,delta*smooth_speed)
+	value=display_value
+
+
+func add_time(sec:float)->void:
+	current_time+=sec
+	current_time=min(current_time,max_time)
+
+func remove_time(sec:float)->void:
+	current_time-=sec
+	current_time=max(current_time,0)
+
+
+func reset_timer()->void:
+	current_time=max_time
+	display_value=max_time
+	value=display_value
