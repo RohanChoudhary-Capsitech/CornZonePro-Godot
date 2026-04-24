eed0c17e4da7368265de2a6b1ab028d8b98bc6c3
Rohan5210 <rohan.choudhary@capsitech.com>
2026-04-24 16:44:57 +0530

    Update timer_slider.gd

--- Scripts/timer_slider.gd
+++ Scripts/timer_slider.gd
@@ -3,6 +3,7 @@ extends TextureProgressBar
 @export var max_time:float=30.0
 @export var smooth_speed:float=30.0
 
+var timer_paused:bool=false
 var current_time:float
 var display_value:float
 
@@ -17,6 +18,8 @@ func _ready() -> void:
 
 
 func _process(delta: float) -> void:
+	if timer_paused:
+		return
 	#Real Timer
 	if current_time>0:
 		current_time-=delta
@@ -35,8 +38,13 @@ func remove_time(sec:float)->void:
 	current_time-=sec
 	current_time=max(current_time,0)
 
-
 func reset_timer()->void:
 	current_time=max_time
 	display_value=max_time
 	value=display_value
+
+func pause_timer()->void:
+	timer_paused=true
+
+func resume_timer()->void:
+	timer_paused=false
