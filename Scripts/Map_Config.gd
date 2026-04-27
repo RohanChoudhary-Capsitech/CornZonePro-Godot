a7f70cee6f4ccb8a4c1299643a32449e02799e10
Rohan5210 <rohan.choudhary@capsitech.com>
2026-04-27 14:23:44 +0530

    materials addon and writing configuration files

---
+++ Scripts/Map_Config.gd
@@ -0,0 +1,7 @@
+extends Resource
+class_name Map_Config
+
+@export var map_name:String=""
+@export var time_limit:float=0.00
+@export var wind_strength:float=0.00
+@export var coin_reward:float=0.00
