68ccc021cd3abe6f8eb04b778015047170b0429b
Chandan kumar parida <chandan.parida@capsitech.com>
2026-04-29 19:59:13 +0530

    privacy button clicked function

--- Scripts/Home/setting_screen.gd
+++ Scripts/Home/setting_screen.gd
@@ -2,3 +2,7 @@ extends CanvasLayer
 
 func _on_cross_button_pressed() -> void:
 	UIManager.toggle_canvas($".")
+
+
+func _on_privacy_policy_button_pressed() -> void:
+	OS.shell_open("https://www.thegamewise.com/privacy-policy/")
