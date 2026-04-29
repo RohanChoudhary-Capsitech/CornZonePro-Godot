3c98adee4a5f2b15427998d49171cd61d47aa183
Chandan kumar parida <chandan.parida@capsitech.com>
2026-04-29 17:09:43 +0530

    rewardPanel ui setup

--- Scripts/Home/login_panel.gd
+++ Scripts/Home/login_panel.gd
@@ -1,5 +1,5 @@
 extends CanvasLayer
 
 func _on_submit_button_pressed() -> void:
-	UIManager.enable_canvas($"../../HomeScreen") 
+	UIManager.enable_canvas($"../HomeScreen") 
 	Prefs.set_int("user",1)
