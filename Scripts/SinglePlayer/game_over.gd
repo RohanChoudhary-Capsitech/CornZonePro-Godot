ab02f005f1df6f3e4544b3ee1fb4d2988f94cd63
Rohan5210 <rohan.choudhary@capsitech.com>
2026-04-30 11:02:55 +0530

    package updates +fixes

--- Scripts/SinglePlayer/game_over.gd
+++ Scripts/SinglePlayer/game_over.gd
@@ -7,8 +7,22 @@ func _ready() -> void:
 
 func gameover()->void:
 	UIManager.enable_canvas($".")
-	$"Control/Black Bg/PausePanel BG/CoinsText".text=str(GameSession.score_p1*2)
+	$"Control/PausePanel BG/CoinsText".text=str(GameSession.score_p1*2)
 
 
 func _on_home_pressed() -> void:
 	UIManager.home()
+
+
+func _on_share_pressed() -> void:
+	print("chla h")
+	$"Control/PausePanel BG/Label2".visible=true
+	if Engine.has_singleton("GodotShare"):
+		var share = Engine.get_singleton("GodotShare")
+		share.shareText("Test from Godot")
+	else:
+		print("Plugin not found")
+
+
+func _on_restart_pressed() -> void:
+	print("mai chala")
