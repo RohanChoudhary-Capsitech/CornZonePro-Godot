d3792fdff0f948b20ec2050636fbc456b4d05298
Rohan5210 <rohan.choudhary@capsitech.com>
2026-05-04 19:00:44 +0530

    toggle issues fixed homescreen

--- Scripts/Home/inventory_screen.gd
+++ Scripts/Home/inventory_screen.gd
@@ -2,8 +2,8 @@ extends CanvasLayer
 
 
 func _on_shop_button_pressed() -> void:
-	UIManager.toggle_canvas($"../ShopScreen")
+	UIManager.enable_canvas($"../ShopScreen")
 
 
 func _on_play_button_pressed() -> void:
-	UIManager.toggle_canvas($".")
+	UIManager.enable_canvas($"../HomeScreen")
--- Scripts/Home/shop_screen.gd
+++ Scripts/Home/shop_screen.gd
@@ -2,8 +2,8 @@ extends CanvasLayer
 
 
 func _on_play_button_pressed() -> void:
-	UIManager.toggle_canvas($".")
+	UIManager.enable_canvas($"../HomeScreen")
 
 
 func _on_inventory_button_pressed() -> void:
-	UIManager.toggle_canvas($"../InventoryScreen")
+	UIManager.enable_canvas($"../InventoryScreen")
