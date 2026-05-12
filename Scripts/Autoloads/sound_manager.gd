080999d9d9fe39e6201088b6a2c7569ad07775a6
Chandan kumar parida <chandan.parida@capsitech.com>
2026-05-12 12:34:30 +0530

    added soundmanager

---
+++ Scripts/Autoloads/sound_manager.gd
@@ -0,0 +1,69 @@
+extends Node
+
+@export_group("Audio sources")
+@onready var bgm_source: AudioStreamPlayer = $BGMSource
+@onready var sfx_source: AudioStreamPlayer = $SFXSource
+
+@export_group("Audio Clips")
+@export var bgm_clip: AudioStream
+@export var button_clip: AudioStream
+@export var bag_collide_clip: AudioStream
+@export var bag_pot_clip: AudioStream
+@export var coin_collect_clip: AudioStream
+@export var gameover_clip: AudioStream
+@export var win_clip: AudioStream
+@export var wind_clip: AudioStream
+@export var powerup_clip: AudioStream
+
+var is_music_on: bool = true
+var is_sound_on: bool = true
+
+func _ready() -> void:
+	load_setting()
+	bgm_source.stream = bgm_clip
+	bgm_source.bus = "Music"
+	play_bgm()
+	
+
+func load_setting():
+	is_music_on = Prefs.get_bool("music")
+	is_sound_on = Prefs.get_bool("sound")
+
+
+func play_bgm():
+	if not is_music_on and bgm_source.playing: return
+	bgm_source.play()
+	
+func stop_bgm():
+	if bgm_source.playing:
+		bgm_source.stop()
+
+
+func play_sfx(clip: AudioStream):
+	if not is_sound_on or clip == null: return
+	sfx_source.stream = clip
+	sfx_source.play()
+	
+func play_button_clicks():
+	play_sfx(button_clip)
+
+func play_bag_collide():
+	play_sfx(bag_collide_clip)
+
+func play_bag_pot():
+	play_sfx(bag_pot_clip)
+
+func play_coin_collect():
+	play_sfx(coin_collect_clip)
+
+func play_game_over():
+	play_sfx(gameover_clip)
+
+func play_win():
+	play_sfx(win_clip)
+	
+func play_wind():
+	play_sfx(wind_clip)
+
+func play_powerup():
+	play_sfx(powerup_clip)
