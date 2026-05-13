18c461636f2361f3f1d7ce1850c2dd51fe3e8f6e
Rohan5210 <rohan.choudhary@capsitech.com>
2026-05-13 11:22:30 +0530

    updated single player fallback for continous gameover working

--- Scripts/SinglePlayer/game_over.gd
+++ Scripts/SinglePlayer/game_over.gd
@@ -1,5 +1,6 @@
 extends CanvasLayer
 
+var is_game_over := false
 @onready var timer_slider := $"../InGame UI/Slider/TimerSlider" as MatchTimerSlider
 @onready var coins_text := $"Control/PausePanel BG/CoinsText" as Label
 
@@ -7,6 +8,12 @@ func _ready() -> void:
 	timer_slider.time_over.connect(gameover)
 
 func gameover()->void:
+	if is_game_over:
+		return
+	
+	is_game_over = true
+	
+	timer_slider.time_over.disconnect(gameover)
 	SoundManager.play_game_over()
 	UIManager.enable_canvas($".")
 	coins_text.text = str(DataManager.get_coins())
